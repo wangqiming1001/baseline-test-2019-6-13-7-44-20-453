58151fa70ba4abec1c51c22855880d2abadc9810
wangqiming1001 <512821390@qq.com>
2019-08-19 18:47:12 +0800

    修改

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+	var collectionC = new Array();
+	for(var i = 0; i < collectionA.length;i++){
+		for(var j = 0; j < collectionB.length;j++){
+			if(collectionA[i] == collectionB[j]){
+				collectionC.push(collectionA[i]);				
+			}
+		}
+		
+	}
+  return collectionC;
 }
--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+	var collectionC = new Array();
+	for(var i = 0; i < collectionA.length;i++){
+		for(var j = 0; j < objectB.value.length;j++){
+			if(collectionA[i].key == objectB.value[j]){
+				collectionC.push(collectionA[i].key);				
+			}
+		}
+	}
+  return collectionC;
 }
+
